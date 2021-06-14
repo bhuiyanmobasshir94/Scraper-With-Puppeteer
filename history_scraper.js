9a51d43652fb946c8883adad015316cbee3553c7
Mobasshir Bhuiyan <mobasshirbhuiyan.shagor@gmail.com>
2021-06-14 15:07:23 +0600

    Update history_scraper.js

--- history_scraper.js
+++ history_scraper.js
@@ -1,3 +1,4 @@
 let allPosts = document.body.querySelectorAll("div.user-account-content-tab-history-infos-part");
 scrapeItems = [];
 allPosts.forEach((item) => { let orderId = item.querySelector("div.f-l span b").innerText; scrapeItems.push({item: item.innerText, orderId: orderId});})
+scrapeItems.forEach((item) => {console.log(item)})
